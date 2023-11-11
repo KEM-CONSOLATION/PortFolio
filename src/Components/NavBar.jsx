80604160418bfb8d9da2626e0b445e6a0274f688
Kem-consolation <consolationlotachi@gmail.com>
2023-11-11 19:41:12 +0100

    Added Cv link

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -2,12 +2,9 @@ import React, { useState } from "react";
 import {
   MailLineIcon,
   GithubLineIcon,
-  TwitterLineIcon,
-  ServiceLineIcon,
-  HomeGearLineIcon,
   Menu1LineIcon,
   CloseLineIcon,
-  SettingsFillIcon,
+  LinkedinFillIcon,
 } from "../assets/icons";
 import { Link } from "react-router-dom";
 const NavBar = () => {
@@ -44,45 +41,26 @@ const NavBar = () => {
           <div className={`md:flex ${!open ? "block" : "hidden"}`}>
             <ul className=" md:flex gap-8 cursor-pointer font-semibold">
               <li className="hover:text-blue-500 mb-5 py-2 px-3 md:py-0 md:px-0 md:mb-0 ">
-                {" "}
-                <Link to="/">
-                  {/* <span className="">
-                    <HomeGearLineIcon className=" text-blue-500 inline  items-center" />
-                  </span>{" "} */}
-                  Home
-                </Link>
+                <Link to="/">Home</Link>
               </li>
               <li className="hover:text-blue-500 mb-5 py-2 px-3 md:py-0 md:px-0 md:mb-0 ">
-                {" "}
-                <Link to="/projects">
-                  {/* <span className="">
-                    <SettingsFillIcon className=" text-blue-500 inline  items-center" />
-                  </span>{" "} */}
-                  Projects
-                </Link>
+                <Link to="/projects">Projects</Link>
               </li>
-
-              {/* <li className="hover:text-blue-500 mb-5 py-2 px-3 md:py-0 md:px-0 md:mb-0 ">
-                {" "}
-                <span className="">
-                  <ServiceLineIcon className=" text-blue-500 inline  items-center" />
-                </span>{" "}
-                Services
-              </li> */}
             </ul>
           </div>
           <div className={`lg:flex ${!open ? "block" : "hidden"}`}>
             <ul className=" flex gap-5 cursor-pointer font-semibold">
-              <li className="hover:text-blue-500 py-2 px-3 md:py-0 md:px-0">
-                <a href="https://twitter.com/TechieConso" target="_blank">
-                  <TwitterLineIcon className="text-blue-500 inline  items-center" />{" "}
-                  {/* Twitter */}
+              <div className="h-[35px] w-[35px] flex justify-center items-center text-white bg-blue-500 rounded-full">
+                <a
+                  href="https://www.linkedin.com/in/kem-consolation/"
+                  target="_blank"
+                >
+                  <LinkedinFillIcon className="" />
                 </a>
-              </li>
+              </div>
               <li className="hover:text-blue-500 py-2 px-3 md:py-0 md:px-0">
                 <a href="https://github.com/KEM-CONSOLATION" target="_blank">
                   <GithubLineIcon className="text-black-500  inline items-center" />{" "}
-                  {/* Github{" "} */}
                 </a>
               </li>
               <li className="hover:text-blue-500 py-2 px-3 md:py-0 md:px-0">
@@ -91,7 +69,6 @@ const NavBar = () => {
                   target="_blank"
                 >
                   <MailLineIcon className="text-red-500  inline items-center" />{" "}
-                  {/* Mail{" "} */}
                 </a>
               </li>
             </ul>
